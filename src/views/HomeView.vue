88b889bf160e12bf67f250072fba693ab0f2ef75
李昊 <1097261154@qq.com>
2023-04-03 02:10:20 +0800

    feat: 初步完工

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,21 +5,25 @@
          <div class="main-left">
             <div class="chart-1" style="width: 90%;height:40%;">
             </div>
+            <div class="chart-1-btn">
+               <el-button type="primary" @click="handleClick">{{ chart1Title }}</el-button>
+            </div>
             <div class="chart-3" style="width: 90%;">
-               <el-table :data="chart3" >
+               <el-table :data="chart3">
                   <el-table-column prop="acc" label="ACC" />
                   <el-table-column prop="auroc" label="AUROC" />
                   <el-table-column prop="auprc" label="AUPRC" />
                   <el-table-column prop="ece" label="ECE" />
                   <el-table-column prop="aece" label="AECE" />
-                  <el-table-column prop="nll" label="NLL"/>
+                  <el-table-column prop="nll" label="NLL" />
                </el-table>
+               <div class="comment">注：±左右分别是性能均值及标准差，单位为%</div>
             </div>
             <div class="chart-4" style="width: 90%;height:30%;">
             </div>
          </div>
          <div class="main-right">
-            <div class="chart-2" style="width: 100%;height:85%;">
+            <div class="chart-2" style="width: 90%;height:85%;">
             </div>
          </div>
       </div>
@@ -42,19 +46,16 @@ const testList = [
    "e_local",
    "i_global",
    "i_local",
-   "年龄",
-   "性别"
 ];
-const pureDimensions = [];
+const pureDimensions = dimensions.filter((item) => !testList.includes(item));
 let chart1 = null;
 let chart2 = null;
 let chart3 = ref([]);
 let chart4 = null;
-dimensions.forEach((item) => {
-   if (!testList.includes(item)) {
-      pureDimensions.push(item);
-   }
-})
+let chart1CurrentOption = {};
+let scatterOption = {};
+let barOption = {};
+const chart1Title = ref('数据总览');
 const checkRange = (value) => {
    let temp;
    if (value < dcDict.bin_min_value) {
@@ -71,20 +72,20 @@ const checkRange = (value) => {
       + 'ECE:' + ece + '<br/>'
       + 'AECE:' + aece + '<br/>'
       + 'NLL:' + nll + '<br/>';
-   return str.replaceAll("@","±");
+   return str.replaceAll("@", "±");
 }
 /* LLLeo's comment: copilot，我滴超人！ */
-const countdataList = ()=>{
+const countdataList = () => {
    let temp = [];
    let low = 0;
    let middle = 0;
    let high = 0;
-   dataList.forEach((item)=>{
-      if(item.data_confidence < dcDict.bin_min_value){
+   dataList.forEach((item) => {
+      if (item.data_confidence < dcDict.bin_min_value) {
          low++;
-      }else if(item.data_confidence > dcDict.bin_max_value){
+      } else if (item.data_confidence > dcDict.bin_max_value) {
          high++;
-      }else{
+      } else {
          middle++;
       }
    })
@@ -93,11 +94,106 @@ const countdataList = ()=>{
    temp.push(high);
    return temp;
 }
+const handleClick = () => {
+   chart1CurrentOption = chart1CurrentOption === scatterOption ? barOption : scatterOption;
+   chart1Title.value = chart1Title.value === '数据总览' ? '散点分布' : '数据总览';
+   chart1.setOption(chart1CurrentOption, true);
+}
+const geteSource = (arr_i, arr_e) => {
+   let temp = [];
+   for (let i = 0; i < pureDimensions.length; i++) {
+      temp.push([
+         pureDimensions[i],
+         arr_i[i],
+         arr_e[i],
+      ])
+   }
+   return temp;
+}
+const checkConfidence = (value, name) => {
+   if (name === '低数据置信度区间') {
+      return value < dcDict.bin_min_value;
+   } else if (name === '中数据置信度区间') {
+      return value > dcDict.bin_min_value && value < dcDict.bin_max_value;
+   } else if (name === '高数据置信度区间') {
+      return value > dcDict.bin_max_value;
+   }
+   return true;
+}
+const changeChart2 = (params) => {
+   let category = [];
+   let data = [];
+   if (params.componentSubType !== 'scatter') {
+      for (let i = 0; i < dataList.length; i++) {
+         let temp = dataList[i];
+         if (temp.classify_result === 1 && checkConfidence(temp.data_confidence, params.name)) {
+            Object.entries(temp).forEach(([key, value]) => {
+               if (!testList.includes(key)) {
+                  category.push(key);
+                  data.push(value);
+               }
+            })
+            break;
+         }
+      }
+      chart2.setOption({
+         title: {
+            text: `${params.name}-预测成功患者综合特征指标`,
+            left: 'center',
+         },
+         yAxis: {
+            type: 'category',
+            data: category,
+            axisLabel: {
+               interval: 1,
+               rotate: -5
+            }
+         },
+         series: [
+            {
+               type: 'bar',
+               data
+            },
+         ],
+      })
+      return;
+   }
+
+   Object.entries(dataList[params.data.testset_pid]).forEach(([key, value]) => {
+      if (key === 'classify_result' && value === 1) {
+
+      }
+      if (!testList.includes(key)) {
+         category.push(key);
+         data.push(value);
+      }
+   })
+   chart2.setOption({
+      title: {
+         text: '患者特征指标',
+         left: 'center',
+      },
+      yAxis: {
+         type: 'category',
+         data: category,
+         axisLabel: {
+            interval: 1,
+            rotate: -5
+         }
+      },
+      series: [
+         {
+            type: 'bar',
+            data
+         },
+      ],
+   })
+};
 const initChart1 = () => {
    chart1 = echarts.init(document.querySelector(".chart-1"));
-   let scatterOption = {
+   scatterOption = {
       title: {
-         text: '数据分布',
+         text: '散点分布',
          left: 'center',
          top: 10,
       },
@@ -111,14 +207,15 @@ const initChart1 = () => {
       },
       toolbox: {
          feature: {
-            dataZoom: {},
-            brush: {
-               type: ['rect', 'polygon', 'clear']
-            }
+            dataZoom: {
+               yAxisIndex: 'none'
+            },
+            dataView:{},
+            saveAsImage:{
+               type:"png",
+               title:"Save as PNG",
+            },
          }
-      },
-      tooltip: {
-         show: true
       },
       dataset: {
          dimensions,
@@ -130,12 +227,13 @@ const initChart1 = () => {
          min: 0, // 需要给出数值范围，最小数值。
          max: 1, // 需要给出数值范围，最大数值。
          inRange: {
-            color: ['#e73c56', '#121122', '#1475e7'],
+            color: ['#e73c56', '#1475e7'],
          }
       },
       series: [
          {
             type: 'scatter',
+            name: "scatter",
             encode: {
                x: ['data_confidence'],
                y: ['preds_all'],
@@ -182,53 +280,120 @@ const initChart1 = () => {
          }
       }
    };
-   chart1.setOption(scatterOption);
-   /* 根据每次的testset_pid来筛选更新对应dataList */
-   chart1.on('mouseover', (params) => {
-      if(params.componentSubType!=='scatter') return;
-      let category = [];
-      let data = [];
-      Object.entries(dataList[params.data.testset_pid]).forEach(([key, value]) => {
-         if (!testList.includes(key)) {
-            category.push(key);
-            data.push(value);
+   barOption = {
+      title: {
+         text: '数据总览',
+         left: 'center',
+         top: 10,
+      },
+      xAxis: {
+         type: "category",
+         data: ["低数据置信度区间", "中数据置信度区间", "高数据置信度区间"]
+      },
+      yAxis: {},
+      toolbox: {
+         feature: {
+            magicType: {
+               type: ['bar', 'line']
+            },
+            saveAsImage:{
+               type:"png",
+               title:"Save as PNG",
+            },
          }
-      })
-      chart2.setOption({
-         yAxis: {
-            type: 'category',
-            data: category,
-            axisLabel: {
-               interval: 1,
-               rotate: -5
+      },
+      series: [
+         {
+            type: "bar",
+            id: "total",
+            data: [
+               {
+                  value: countdataList()[0],
+                  groupId: "low",
+                  label: {
+                     show: true,
+                     position: "top",
+                     formatter: "{c}"
+                  },
+                  itemStyle: {
+                     color: "#e73c56"
+                  }
+               },
+               {
+                  value: countdataList()[1],
+                  groupId: "middle",
+                  label: {
+                     show: true,
+                     position: "top",
+                     formatter: "{c}"
+                  },
+                  itemStyle: {
+                     color: "#f0f418"
+                  }
+               },
+               {
+                  value: countdataList()[2],
+                  groupId: "high",
+                  label: {
+                     show: true,
+                     position: "top",
+                     formatter: "{c}"
+                  },
+                  itemStyle: {
+                     color: "#87f458"
+                  }
+               }
+            ],
+            universalTransition: {
+               enabled: true,
+               seriesKey: ['low', 'middle', 'high'],
+               delay: function (idx, count) {
+                  return Math.random() * 400;
+               }
             }
-         },
-         series: [
-            {
-               type: 'bar',
-               data
-            }
-         ],
-      })
-   });
+         }
+      ]
+   };
+   chart1CurrentOption = scatterOption;
+   chart1.setOption(chart1CurrentOption, true);
+   /* 根据每次的testset_pid来筛选更新对应dataList */
+   chart1.on('mouseover', changeChart2);
 }
 /* LLLeo's comment: 生成scatter的对应点数据属性 */
 const initChart2 = () => {
    chart2 = echarts.init(document.querySelector(".chart-2"));
    let option = {
       title: {
-         text: '对应数据属性',
+         text: '患者特征指标',
          left: 'center',
       },
       tooltip: {
          trigger: 'axis',
          axisPointer: {
             type: 'shadow'
+         },
+         formatter: function (params) {
+            return (
+               params[0].name + ':' +
+               params[0].data + '<br/>' +
+               'e_local:' + dataList[0].e_local[params[0].dataIndex]
+            );
+         }
+      },
+      toolbox: {
+         feature: {
+            magicType: {
+               type: ['bar', 'line']
+            },
+            saveAsImage:{
+               type:"png",
+               title:"Save as PNG",
+            },
          }
       },
       legend: {},
       grid: {
-         containLabel: true
+         containLabel: true,
       },
       dataset: {
          source: [],
@@ -253,61 +418,83 @@ const initChart2 = () => {
 const initChart3 = () => {
    let temp = performanceList.all;
    Object.entries(temp).forEach(([key, value]) => {
-      temp[key] = value.replaceAll("@","±");
+      temp[key] = value.replaceAll("@", "±");
    })
    chart3.value.push(temp);
 }
 const initChart4 = () => {
    chart4 = echarts.init(document.querySelector(".chart-4"));
-   let barDataList = countdataList();
-   let barOption = {
+   let eSource = geteSource(dataList[0].i_global, dataList[0].e_global);
+   let pieOption = {
       title: {
-         text: '置信度分布',
+         text: '全局特征重要性与存在比率',
          left: 'center',
-         top: 10,
       },
-      xAxis:{
-         type:"category",
-         data:["低置信度","中置信度","高置信度"]
-      },
-      yAxis:{},
+      dataset: [
+         {
+            dimensions: ["name", "i_global", "e_global"],
+            source: eSource,
+         },
+         {
+            transform: {
+               type: "sort",
+               config: {
+                  dimension: "e_global",
+                  order: "desc",
+               }
+            }
+         }
+      ],
       toolbox: {
          feature: {
-            myTool:{
-               show:true,
-               title:"切换为散点图",
-            }
+            dataZoom: {
+               yAxisIndex: 'none'
+            },
+            dataView:{},
+            magicType: {
+               type: ['bar', 'line']
+            },
+            saveAsImage:{
+               type:"png",
+               title:"Save as PNG",
+            },
          }
       },
-      series:[
+      xAxis: {
+         type: 'category',
+         axisLabel: { rotate: 30 }
+      },
+      yAxis: {},
+      series: [
          {
-            type:"bar",
-            id:"total",
-            data:[
-               {
-                  value:barDataList[0],
-                  groupId:"low"
-               },
-               {
-                  value:barDataList[1],
-                  groupId:"middle"
-               },
-               {
-                  value:barDataList[2],
-                  groupId:"high"
-               }
-            ],
-            universalTransition: {
-               enabled: true,
-               seriesKey: ['low', 'middle', 'high'],
-               delay: function (idx, count) {
-                  return Math.random() * 400;
-               }
-            }
+            type: "bar",
+            encode: {
+               x: "name",
+               y: "i_global",
+            },
+         },
+         {
+            type: "bar",
+            encode: {
+               x: "name",
+               y: "e_global",
+            },
          }
-      ]
+      ],
+      tooltip: {
+         formatter: function (params) {
+            console.log("PPP1", params);
+            return (
+               params.data[0] + '<br/>' +
+               'i_global: ' +
+               params.data[1] + '<br/>' +
+               'e_global: ' +
+               params.data[2]
+            );
+         }
+      }
    }
-   chart4.setOption(barOption);
+   chart4.setOption(pieOption);
 }
 onMounted(() => {
    initChart1();
@@ -319,7 +506,6 @@ onMounted(() => {
       chart2.resize();
       chart4.resize();
    })
-   console.log("onMounted",dataList);
 })
 </script>
 <style lang="scss" scoped>
@@ -334,22 +520,41 @@ onMounted(() => {
    overflow: hidden;
    background-color: white;
    height: 100%;
+
    .main-left {
       width: 50%;
       height: 100%;
       box-shadow: 0 1px 4px #00000014;
       margin: auto;
-      .chart-1{
+
+      .chart-1 {
          margin: auto;
       }
-      .chart-3{
+
+      .chart-1-btn {
+         margin: auto;
+         display: flex;
+         justify-content: center;
+         position: relative;
+         top: -20px;
+      }
+
+      .chart-3 {
          margin: auto;
       }
-      .chart-4{
+
+      .comment {
+         color: gray;
+         font-size: 12px;
+         margin-left: 5px;
+      }
+
+      .chart-4 {
          margin: auto;
-         margin-top: 20px;
+         margin-top: 10px;
       }
    }
+
    .main-right {
       width: 50%;
       height: 100%;
@@ -357,11 +562,11 @@ onMounted(() => {
       display: flex;
       justify-content: center;
       margin: auto;
-      .chart-1 {
-         margin: auto;
-         margin-top: 20px;
+      .chart-2{
+         // margin: auto;
+         margin-top: 0;
+         margin-left: -10%;
       }
    }
-   
-}
-</style>
+
+}</style>
