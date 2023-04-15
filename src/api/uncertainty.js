991cb32ff12bf94a3f7aa6a71c822d709032ff9a
Harbour% <2358272468@qq.com>
2023-04-15 13:53:39 +0800

    feat: 不公平性

--- src/api/uncertainty.js
+++ src/api/uncertainty.js
@@ -1,10 +1,37 @@
 import service from "../http/request";
 const url = {
     uncertainty_process: '/uncertain/uncertainty_process',
+    unfairness_process: '/uncertain/unfairness_process',
+    mutil_unfairness_process: '/uncertain/mutil_unfairness_process',
+    all_unfairness_process: '/uncertain/all_unfairness_process'
 }
 export class Uncertain {
     static async UncertainProcess(data) {
         return await service(url.uncertainty_process, {
+            method: "post",
+            responseType: "json",
+            data,
+        });
+    }
+
+    static async UnFairnessProcess(data) {
+        return await service(url.unfairness_process, {
+            method: "post",
+            responseType: "json",
+            data,
+        });
+    }
+
+    static async MultUnFairnessProcess(data) {
+        return await service(url.mutil_unfairness_process, {
+            method: "post",
+            responseType: "json",
+            data,
+        });
+    }
+
+    static async AllUnFairnessProcess(data) {
+        return await service(url.all_unfairness_process, {
             method: "post",
             responseType: "json",
             data,
